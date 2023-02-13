668271632dafc9d803cde7c08ed0e3198ec40722
Giussep Ricardo <giussepr@gmail.com>
2023-02-13 11:58:10 -0500

    Initial commit

---
+++ app/src/main/java/com/giussepr/cashtrack/ui/theme/Shape.kt
@@ -0,0 +1,16 @@
+/*
+ * Created by Giussep Ricardo on 03/01/2023
+ * Copyright (c) 2023 . All rights reserved.
+ */
+
+package com.giussepr.cashtrack.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(6.dp),
+    large = RoundedCornerShape(8.dp)
+)
